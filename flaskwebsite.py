074853fe5c9c46d7f489e4c245949c2e4e24923d
Pawan K <68584821+PawanKatwe@users.noreply.github.com>
2021-11-20 13:30:22 +0530

    Add files via upload

--- flaskwebsite.py
+++ flaskwebsite.py
@@ -7,9 +7,25 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 @app.route("/<name>")
 def user(name):
-    return f"Hello {name}!"
+    return f"Hey! page '{name}' not found on this site"
 
 @app.route("/admin")
 def admin():
